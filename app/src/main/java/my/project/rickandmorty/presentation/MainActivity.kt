8aa705ce20f4994f36b33a3759e42105c757ef2b
Erkesh <dedeeverkesh@gmail.com>
2022-04-17 18:30:36 +0300

    Added navController in MainActivity.kt

--- app/src/main/java/my/project/rickandmorty/presentation/MainActivity.kt
+++ app/src/main/java/my/project/rickandmorty/presentation/MainActivity.kt
@@ -2,11 +2,26 @@ package my.project.rickandmorty.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import my.project.rickandmorty.R
 
 class MainActivity : AppCompatActivity() {
+
+    var navController: NavController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+    navController = findNavController(R.id.navHostFragment)
+
+    setupActionBarWithNavController(navController!!)
+}
+
+override fun onSupportNavigateUp(): Boolean {
+    return navController!!.navigateUp() || super.onSupportNavigateUp()
+}
 }
