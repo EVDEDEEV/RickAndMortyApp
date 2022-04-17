0dcc0ede4363082b07235ba9fc6e8d5f4a73184f
Erkesh <dedeeverkesh@gmail.com>
2022-04-17 17:20:56 +0300

    Added and filled SharedViewModelFactory.kt

---
+++ app/src/main/java/my/project/rickandmorty/presentation/viewModel/SharedViewModelFactory.kt
@@ -0,0 +1,11 @@
+package my.project.rickandmorty.presentation.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import my.project.rickandmorty.data.repository.Repository
+
+class SharedViewModelFactory(private val repository: Repository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return SharedViewModel(repository) as T
+    }
+}
