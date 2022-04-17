fd3ee3598efee3652762ace6ce59fbd6c56987a1
Erkesh <dedeeverkesh@gmail.com>
2022-04-17 15:56:37 +0300

    Added and filled LocationDataModel.kt

--- app/src/main/java/my/project/rickandmorty/data/models/CharacterModel.kt
+++ app/src/main/java/my/project/rickandmorty/data/models/CharacterModel.kt
@@ -1,17 +1,17 @@
 package my.project.rickandmorty.data.models
 
+import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class CharacterModel {
+data class CharacterModel (
     var id: Int,
     var name:String,
     var status:String,
     var species:String,
     var gender:String,
-    var origin:LocationData,
-    var location: LocationData,
+    var origin:LocationDataModel,
+    var location: LocationDataModel,
     var image:String,
     var episode: List<String>
     ): Parcelable
-}
---
+++ app/src/main/java/my/project/rickandmorty/data/models/LocationDataModel.kt
@@ -0,0 +1,12 @@
+package my.project.rickandmorty.data.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class LocationDataModel(
+
+    var name: String,
+) : Parcelable
+
+
